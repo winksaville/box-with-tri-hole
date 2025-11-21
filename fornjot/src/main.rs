f4630ea928cfdca512ca4b9045ddf8f9edf494e7
Wink Saville <wink@saville.com>
2025-11-21 14:57:57 -0800

    feat: Simplify hardcode box parameters
     - Removed clap
     - set the x, y, z parameters to 40, 40, 15

--- fornjot/src/main.rs
+++ fornjot/src/main.rs
@@ -1,29 +1,10 @@
-use clap::Parser;
-
-#[derive(Parser)]
-struct Parameters {
-    /// Size of the cuboid along the x-axis
-    #[arg(long, default_value = "3.0")]
-    x: f64,
-
-    /// Size of the cuboid along the y-axis
-    #[arg(long, default_value = "2.0")]
-    y: f64,
-
-    /// Size of the cuboid along the z-axis
-    #[arg(long, default_value = "1.0")]
-    z: f64,
-
-    #[command(flatten)]
-    fj: fj::Args,
-}
-
 fn main() -> fj::Result {
     let mut fj = fj::Instance::new();
-    let params = Parameters::parse();
+    let args = fj::Args::parse();
 
-    let model = cuboid::model([params.x, params.y, params.z], &mut fj.core);
-    fj.process_model_args(&model, params.fj)?;
+    // Hard-coded dimensions: 40x40x15mm box
+    let model = cuboid::model([40.0, 40.0, 15.0], &mut fj.core);
+    fj.process_model_args(&model, args)?;
 
     Ok(())
 }
